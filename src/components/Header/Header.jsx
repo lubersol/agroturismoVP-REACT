f207c54c434563160c62f6bda6a09982ae181f66
Lucia <lubersol@gmail.com>
2021-01-09 20:48:09 +0100

    añadido png logo varitx paradise

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 //import { Button, notification } from 'antd';
 import './Header.css';
-import React from "react";
 import {Link} from 'react-router-dom';
 
 // class component
