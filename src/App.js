f6eff46464b0e3f46cd0c04eac68047b21c2b1ce
Lucia <lubersol@gmail.com>
2021-01-09 13:24:47 +0100

    agregadas rutas archivo app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
+import 'antd/dist/antd.css';
+
+//Importación de componentes
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+
+//Importación de containers
+import Home from './containers/Home/Home';
+import Register from './containers/Register/Register';
+import Login from './containers/Login/Login';
+import Rooms from './containers/Rooms/Rooms';
+import Gallery from './containers/Gallery/Gallery';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Switch>
+        <Route path='/' component={Home} exact />
+        <Route path='/register' component={Register} exact />
+        <Route path='/login' component={Login} exact />
+        <Route path='/rooms' component={Rooms} exact />
+        <Route path='/gallery' component={Gallery} exact />
+      </Switch>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
